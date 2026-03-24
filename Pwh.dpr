b08c8a7295a48f72dcff74167a096fde20e1e57f
black-chest <doni.rusdiyatno87@gmail.com>
2026-03-24 23:06:47 +0700

    baru ini

--- Pwh.dpr
+++ Pwh.dpr
@@ -2,7 +2,8 @@ program Pwh;
 
 uses
   Vcl.Forms,
-  Uwh in 'Uwh.pas' {Form1};
+  Uwh in 'Uwh.pas' {Form1},
+  U2 in 'U2.pas' {Form2};
 
 {$R *.res}
 
@@ -10,5 +11,7 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TForm2, Form2);
+  Application.CreateForm(TForm2, Form2);
   Application.Run;
 end.
