6112484cf3a837bb576afc00b579c69831e043bb
black-chest <doni.rusdiyatno87@gmail.com>
2026-03-25 21:13:58 +0700

    Ini adalah file awal proyek warehouse

---
+++ PWarehouse.dpr
@@ -0,0 +1,14 @@
+program PWarehouse;
+
+uses
+  Vcl.Forms,
+  UWarehouse in 'UWarehouse.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
